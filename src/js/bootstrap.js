172cbef67ec5c26893487516c2245b7fa91d959f
Arkanic <silverfishasaurus@gmail.com>
2021-07-25 04:55:04 +0000

    webpack fix

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -1,2 +1,4 @@
+import "../css/style.css";
+
 /* async */
 import("./index.js").catch(e => console.error("Error importing main script: ", e));
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,73 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const path = require('path');
 
 module.exports = {
     entry: "./src/js/bootstrap.js",
+    mode: "production",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bootstrap.js",
+        filename: "[contenthash].js",
     },
-    mode: "production",
+    stats: {
+        children: true
+    },
     experiments: {
         asyncWebAssembly: true
     },
+    optimization: {
+        splitChunks: {
+            chunks: "all"
+        },
+        moduleIds: "deterministic",
+        minimizer: [
+            new TerserJSPlugin({})
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: "/node_modules/",
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env"
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    "css-loader"
+                ]
+            }
+        ]
+    },
     plugins: [
-        new CopyWebpackPlugin({
+        /*new CopyWebpackPlugin({
             patterns: [
                 {
                     from: "./src/static/",
                     to: "."
                 }
             ]
+        }),*/
+        new MiniCssExtractPlugin({
+            filename: "[contenthash].css"
+        }),
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: "src/templates/index.html",
+            inject: "body"
         }),
         new CleanWebpackPlugin({})
     ],
